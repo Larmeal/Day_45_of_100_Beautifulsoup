ccb8f23c69f813122b46faab80e9065d6721dd6b
Larmeal <armloveyou252@hotmail.com>
2022-03-04 02:09:14 +0700

    easy

--- bs4-start/project_today.py
+++ bs4-start/project_today.py
@@ -8,7 +8,7 @@ soup = BeautifulSoup(source, "html.parser")
 
 movies_tag = [tag.getText() for tag in soup.find_all(name="h3", class_="title")][::-1]
 
-with open("bs4-start\list_movie.txt", "a", encoding="utf-8") as file:
+with open("bs4-start\list_movie.txt", "w", encoding="utf-8") as file:
     for movie in movies_tag:
-        file.write(str(movie) + "\n")
+        file.write(movie + "\n")
         
